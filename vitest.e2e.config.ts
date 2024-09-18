9c2385fc4d3ed7201a8779ebd65d1a12f773a5e5
Ella Robinson <ellaroosmarijn@gmail.com>
2024-09-18 16:58:53 +0400

    fix: add 'fileParallelism: false' to stop tests running in parallel

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -5,6 +5,7 @@ export default mergeConfig(
   vitestConfig,
   defineConfig({
     test: {
+      fileParallelism: false,
       include: ['**/*.e2e-{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
       environmentMatchGlobs: [['src/**', 'prisma']],
     },
