eeabd01545607aa78f06788e2f6a86191db17c42
Ella Robinson <ellaroosmarijn@gmail.com>
2024-10-03 21:49:58 +0400

    feat: change delete route to soft delete by updating deleted_at field

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -28,20 +28,25 @@ export async function DELETE(
   req: Request,
   { params }: { params: { id: string } },
 ) {
+  const id = parseInt(params.id)
 
-  const id = parseInt(params.id);
-  
   try {
-    const deletedEvent = await prisma.event.delete({
+    const deletedEvent = await prisma.event.update({
       where: {
         id: id,
       },
-    });
+      data: {
+        deleted_at: new Date(),
+      },
+    })
 
-    return new Response(JSON.stringify({ message: 'Event deleted successfully', deletedEvent }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return new Response(
+      JSON.stringify({ message: "Event deleted successfully", deletedEvent }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    )
   } catch (error) {
     return new Response(JSON.stringify({ error: 'Event not found or deletion failed' }), {
       status: 404,
