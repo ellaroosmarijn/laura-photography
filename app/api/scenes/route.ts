4c2bd163373ee6de26ef0ae3b4b979a8142ed8c0
Ella Robinson <ellaroosmarijn@gmail.com>
2024-10-05 10:58:57 +0400

    feat: add post route for creating a scene

--- app/api/scenes/route.ts
+++ app/api/scenes/route.ts
@@ -1,6 +1,7 @@
 // get all scenes (all scenes or all scenes within an event?)
 
 import { PrismaClient } from "@prisma/client"
+import { createErrorResponse } from "tests/utlis/route-helpers"
 
 const prisma = new PrismaClient()
 
@@ -17,5 +18,32 @@ export function GET() {
       status: 500,
       headers: { "Content-Type": "application/json" },
     })
+  }
+}
+
+export async function POST(req: Request) {
+  const { name, eventId } = await req.json()
+  try {
+    if (!name || typeof name !== "string" || name.trim().length === 0) {
+      return createErrorResponse("Invalid input: name cannot be empty", 400)
+    }
+
+    if (!eventId) {
+      return createErrorResponse(
+        "Invalid input: scene must be linked to an event",
+        400,
+      )
+    }
+
+    const createdScene = await prisma.scene.create({
+      data: { name, event_id: eventId },
+    })
+
+    return new Response(JSON.stringify({ createdScene }), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error) {
+    return createErrorResponse("Creating scene failed", 500)
   }
 }
