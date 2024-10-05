1880957986bb9642b6b8c0c1549cb83055191108
Ella Robinson <ellaroosmarijn@gmail.com>
2024-10-05 10:19:10 +0400

    rename data to fetchedEvents for readibility & format document

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,25 +1,22 @@
-
 // For getting all event, creating an event when you doen't know the id
 // don't forget about soft deleted events
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export async function GET() {
-  try{
-    const data = await prisma.event.findMany()
+  try {
+    const fetchedEvents = await prisma.event.findMany()
 
-    return new Response(JSON.stringify({ data }), {
+    return new Response(JSON.stringify({ fetchedEvents }), {
       status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-
+      headers: { "Content-Type": "application/json" },
+    })
   } catch (error) {
-    console.error('Error fetching events:', error);
-    return new Response(JSON.stringify({ error: 'Error fetching events' }), {
+    return new Response(JSON.stringify({ error: "Error fetching events" }), {
       status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
+      headers: { "Content-Type": "application/json" },
+    })
   }
 }
