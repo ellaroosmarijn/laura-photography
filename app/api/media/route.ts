67aa60a2fe6235f7ff3962d48e0d9d1b1ee25206
Ella Robinson <ellaroosmarijn@gmail.com>
2024-10-10 11:37:50 +0400

    feat: add a get all media route

--- app/api/media/route.ts
+++ app/api/media/route.ts
@@ -1,1 +1,19 @@
 // TODO: clarify - get all media (all media and/or all media within a scene???)
+
+import { PrismaClient } from "@prisma/client"
+import { createErrorResponse } from "utils/route-helpers"
+
+const prisma = new PrismaClient()
+
+export function GET() {
+  try {
+    const fetchedMedia = prisma.media.findMany({ where: { deleted_at: null } })
+
+    return new Response(JSON.stringify({ fetchedMedia }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error) {
+    return createErrorResponse("Error fetching media", 500)
+  }
+}
