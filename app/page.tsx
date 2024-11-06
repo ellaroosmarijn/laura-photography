9989a02b7e7dfd81a709d4e76313c0f8788bbbd0
Ella Robinson <ellaroosmarijn@gmail.com>
2024-11-06 09:55:06 +0400

    update page to have gallery hero and header components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import { GalleryHero } from "components/gallery-hero"
+import { Header } from "components/header"
+
 export default function Page() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <GalleryHero
+        alt="hero image test"
+        src={"/LR.jpg"}
+        title="Ellen & Dave - Davenport House"
+        subtitle="May 18, 2024"
+      />
+      <Header />
+    </>
+  )
 }
