6f3bf191812e8c6b263cdb27ba127f950f52663b
Ella Robinson <ellaroosmarijn@gmail.com>
2024-11-06 09:53:17 +0400

    fix: body to be full width and height

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ width: "100%", height: "100%", margin: 0, padding: 0 }}>
+        {children}
+      </body>
     </html>
   )
 }
