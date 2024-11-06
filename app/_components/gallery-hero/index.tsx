f81ba5d3baed7f55126cd6d0435e992f42b0c063
Ella Robinson <ellaroosmarijn@gmail.com>
2024-11-06 09:54:12 +0400

    refactor: to use Image tag from next image

--- app/_components/gallery-hero/index.tsx
+++ app/_components/gallery-hero/index.tsx
@@ -1,6 +1,7 @@
 import { ChevronDown } from "lucide-react"
 import styles from "./index.module.css"
 import Link from "next/link"
+import Image from "next/image"
 
 type GalleryHeroProps = {
   alt: string
@@ -12,7 +13,13 @@ type GalleryHeroProps = {
 export function GalleryHero({ alt, src, title, subtitle }: GalleryHeroProps) {
   return (
     <div className={styles.heroContainer}>
-      <img className={styles.image} alt={alt} src={src} />
+      <Image
+        className={styles.image}
+        alt={alt}
+        src={src}
+        width={2000}
+        height={1333}
+      />
       <div className={styles.textOverlay}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
