e924b2c55a2b778b9234b773880cd3c85b138afb
Ella Robinson <ellaroosmarijn@gmail.com>
2024-11-15 20:31:12 +0500

    feat: basic dropdown structure

---
+++ app/_components/dropdown/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+type DropdownProps = {
+  triggerText: string
+  contents: string[]
+}
+
+export function Dropdown({ triggerText, contents }: DropdownProps) {
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <div onClick={() => setOpen(true)}>{triggerText}</div>
+      {contents &&
+        open === true &&
+        contents.map((content, index) => <div key={index}>{content}</div>)}
+    </>
+  )
+}
