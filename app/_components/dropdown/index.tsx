39c5ebaef2459b5681cc399332a6619d41d691f5
Ella Robinson <ellaroosmarijn@gmail.com>
2024-11-16 20:15:36 +0500

    fix: contents to have onClick and only show when open is true

--- app/_components/dropdown/index.tsx
+++ app/_components/dropdown/index.tsx
@@ -4,17 +4,24 @@ import { useState } from "react"
 
 type DropdownProps = {
   triggerText: string
-  contents: string[]
+  contents: { text: string; onClick: () => void }[]
 }
 
 export function Dropdown({ triggerText, contents }: DropdownProps) {
   const [open, setOpen] = useState(false)
+
   return (
     <>
       <div onClick={() => setOpen(true)}>{triggerText}</div>
-      {contents &&
-        open === true &&
-        contents.map((content, index) => <div key={index}>{content}</div>)}
+      {open === true && (
+        <div>
+          {contents.map((content, index) => (
+            <button key={index} onClick={content.onClick}>
+              {content.text}
+            </button>
+          ))}
+        </div>
+      )}
     </>
   )
 }
