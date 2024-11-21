f67bcedfe1d6ab6aa3d67a4f0c95b8d507a595c3
Ella Robinson <ellaroosmarijn@gmail.com>
2024-11-21 21:16:38 +0500

    fix: page overscrolling by adding overscroll none

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{ width: "100%", height: "100%", margin: 0, padding: 0 }}>
+      <body
+        style={{
+          width: "100%",
+          height: "100%",
+          margin: 0,
+          padding: 0,
+          overscrollBehavior: "none",
+        }}
+      >
         {children}
       </body>
     </html>
