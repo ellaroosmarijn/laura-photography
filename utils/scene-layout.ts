0aa2ebe5deeecddfc9f9bf562820bc13d2147e43
Ella Robinson <ellaroosmarijn@gmail.com>
2024-12-03 15:09:31 +0530

    feat: add minimum distance since last two span image condition

--- utils/scene-layout.ts
+++ utils/scene-layout.ts
@@ -11,6 +11,8 @@ export function calculateSceneLayout(
   const columnHeights = new Array(totalNumberOfColumns).fill(0)
   const placedImagesInLayout = []
   const images = [...images_]
+  const distanceSinceLastTwoSpan = new Array(totalNumberOfColumns).fill(0)
+  const minDistanceForTwoSpan = 1000
 
   console.log(">>>>>>>", columnHeights)
 
@@ -36,6 +38,7 @@ export function calculateSceneLayout(
       i++
     ) {
       columnHeights[i] += layoutImageHeight
+      distanceSinceLastTwoSpan[i] = 0
     }
 
     placedImagesInLayout.push({
@@ -61,23 +64,42 @@ export function calculateSceneLayout(
     )
 
     if (matchingIndex !== -1) {
-      placeAnImage(2, matchingIndex)
+      if (
+        Math.min(
+          ...distanceSinceLastTwoSpan.slice(matchingIndex, matchingIndex + 2),
+        ) >= minDistanceForTwoSpan
+      ) {
+        placeAnImage(2, matchingIndex)
 
-      const imageAspectRatio =
-        images[0].res.high.width / images[0].res.high.height
-      const columnWidth = layoutWidth / totalNumberOfColumns
-      const layoutImageWidth = columnWidth * 2
-      const layoutImageHeight = layoutImageWidth / imageAspectRatio
+        const imageAspectRatio =
+          images[0].res.high.width / images[0].res.high.height
+        const columnWidth = layoutWidth / totalNumberOfColumns
+        const layoutImageWidth = columnWidth * 2
+        const layoutImageHeight = layoutImageWidth / imageAspectRatio
 
-      columnHeights[matchingIndex] += layoutImageHeight
-      columnHeights[matchingIndex + 1] += layoutImageHeight
-    }
-  } else {
-    for (let i = 0; i < images.length; i++) {
-      const shortestColumnIndex = columnHeights.indexOf(
-        Math.min(...columnHeights),
-      )
-      placeAnImage(1, shortestColumnIndex)
+        columnHeights[matchingIndex] += layoutImageHeight
+        columnHeights[matchingIndex + 1] += layoutImageHeight
+
+        distanceSinceLastTwoSpan[matchingIndex] = 0
+        distanceSinceLastTwoSpan[matchingIndex + 1] = 0
+      }
+    } else {
+      // column 0, 1, 2
+      // span >= 1
+      for (let i = 0; i < images.length; i++) {
+        const shortestColumnIndex = columnHeights.indexOf(
+          Math.min(...columnHeights),
+        )
+        const imageAspectRatio =
+          images[i].res.high.width / images[i].res.high.height
+        const columnWidth = layoutWidth / totalNumberOfColumns
+        const layoutImageWidth = columnWidth * 1
+        const layoutImageHeight = layoutImageWidth / imageAspectRatio
+
+        placeAnImage(1, shortestColumnIndex)
+
+        distanceSinceLastTwoSpan[shortestColumnIndex] += layoutImageHeight
+      }
     }
   }
 
