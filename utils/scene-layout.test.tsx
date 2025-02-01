53c1a5c47f0ec89cb39585880b470b1604b0f878
Ella Robinson <ellaroosmarijn@gmail.com>
2025-02-01 14:53:04 +0700

    fix scene layout test

--- utils/scene-layout.test.tsx
+++ utils/scene-layout.test.tsx
@@ -1,59 +1,101 @@
 // check bottom height of each image + gap
-// if any two adjacent imges have the same bottom height, then place a span-2
+// if any two adjacent images have the same bottom height, then place a span-2
 // check for the shortest column, then place a span-1 image in it
 // save in a variable bottom height of last span-2 image, only add another when it is X-px below that.
-// Turn off check for bottom height is equal of two adjacent imgages if teh bottom height is 0 (first row)
+// Turn off check for bottom height is equal of two adjacent imgages if the bottom height is 0 (first row)
 import { calculateSceneLayout } from "./scene-layout"
 
-const createMockSceneImage = (id: string, width: number, height: number) => {
-  const lowWidth = width / 2
-  const lowHeight = height / 2
-  const midWidth = width / 1.5
-  const midHeight = height / 1.5
+const createMockSceneImages = (
+  dimensions: [width: number, height: number][],
+) => {
+  return dimensions.map(([width, height], id) => {
+    const lowWidth = width / 2
+    const lowHeight = height / 2
+    const midWidth = width / 1.5
+    const midHeight = height / 1.5
 
-  return {
-    id: id,
-    alt: "",
-    res: {
-      low: {
-        src: `https://picsum.photos/id/866/${lowWidth}/${lowHeight}.jpg`,
-        width: lowWidth,
-        height: lowHeight,
+    return {
+      id: id.toString(),
+      alt: "",
+      res: {
+        low: {
+          src: `https://picsum.photos/id/866/${lowWidth}/${lowHeight}.jpg`,
+          width: lowWidth,
+          height: lowHeight,
+        },
+        mid: {
+          src: `https://picsum.photos/id/866/${midWidth}/${midHeight}.jpg`,
+          width: midWidth,
+          height: midHeight,
+        },
+        high: {
+          src: `https://picsum.photos/id/866/${width}/${height}.jpg`,
+          width,
+          height,
+        },
       },
-      mid: {
-        src: `https://picsum.photos/id/866/${midWidth}/${midHeight}.jpg`,
-        width: midWidth,
-        height: midHeight,
-      },
-      high: {
-        src: `https://picsum.photos/id/866/${width}/${height}.jpg`,
-        width,
-        height,
-      },
-    },
-  }
+    }
+  })
 }
-
-const imageDimensions = [
-  [850, 333],
-  [850, 333],
-  [850, 333],
-  [850, 333],
-  [850, 333],
-  [850, 333],
-  [850, 333],
-]
-
-const images = imageDimensions.map((dimensions, i) =>
-  createMockSceneImage(i.toString(), dimensions[0], dimensions[1]),
-)
 
 describe("scene layout", () => {
   it("calculateSceneLayout", () => {
-    const returnedValue = calculateSceneLayout(images, {
+    const images = createMockSceneImages([
+      [850, 333],
+      [850, 333],
+      [850, 333],
+      [850, 333],
+      [850, 333],
+      [850, 333],
+      [850, 333],
+    ])
+    const processedImages = calculateSceneLayout(images, {
       totalNumberOfColumns: 3,
     })
 
-    console.log(returnedValue)
+    expect(processedImages).toMatchObject([
+      {
+        leftPosition: 0,
+        topPosition: 0,
+        imageAspectRatio: 2.5525525525525525,
+        layoutImageWidth: 33.333333333333336,
+      },
+      {
+        leftPosition: 33.333333333333336,
+        topPosition: 0,
+        imageAspectRatio: 2.5525525525525525,
+        layoutImageWidth: 33.333333333333336,
+      },
+      {
+        leftPosition: 66.66666666666667,
+        topPosition: 0,
+        imageAspectRatio: 2.5525525525525525,
+        layoutImageWidth: 33.333333333333336,
+      },
+      {
+        leftPosition: 0,
+        topPosition: 13.058823529411766,
+        imageAspectRatio: 2.5525525525525525,
+        layoutImageWidth: 33.333333333333336,
+      },
+      {
+        leftPosition: 33.333333333333336,
+        topPosition: 13.058823529411766,
+        imageAspectRatio: 2.5525525525525525,
+        layoutImageWidth: 33.333333333333336,
+      },
+      {
+        leftPosition: 66.66666666666667,
+        topPosition: 13.058823529411766,
+        imageAspectRatio: 2.5525525525525525,
+        layoutImageWidth: 33.333333333333336,
+      },
+      {
+        leftPosition: 0,
+        topPosition: 26.117647058823533,
+        imageAspectRatio: 2.5525525525525525,
+        layoutImageWidth: 33.333333333333336,
+      },
+    ])
   })
 })
